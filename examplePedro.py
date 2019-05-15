fa94e3227a6eee9179f2a6ee9af09567b1520983
Pedro Cunial <pedrocc4@al.insper.edu.br>
2019-05-15 07:41:29 -0300

    event.py and examplePedro.py

--- examplePedro.py
+++ examplePedro.py
@@ -4,7 +4,11 @@ from strategy import Strategy
 from order import Order
 
 
-class PedroStrategy(Strategy):
+STOCK = 'stock'
+COIN = 'coin'
+
+
+class SAR(Strategy):
 
     def __init__(self):
         self.sar = []
@@ -30,10 +34,11 @@ class PedroStrategy(Strategy):
                 if sar_predict > price:
                     self.crescent = False
                     if self.buying == 1:
-                        orders += [Order(-1, 0), Order(-1, 0)]
+                        orders += [Order(event.instrument, -1, 0),
+                                   Order(event.instrument, -1, 0)]
                         self.buying = -1
                     elif self.buying == 0:
-                        orders.append(Order(-1, 0))
+                        orders.append(Order(event.instrument, -1, 0))
                         self.buying = -1
                     self.accel = self.accel_min
                 else:
@@ -44,10 +49,11 @@ class PedroStrategy(Strategy):
                 if sar_predict < price:
                     self.crescent = True
                     if self.buying == -1:
-                        orders += [Order(1, 0), Order(1, 0)]
+                        orders += [Order(event.instrument, 1, 0),
+                                   Order(event.instrument, 1, 0)]
                         self.buying = 1
                     elif self.buying == 0:
-                        orders.append(Order(1, 0))
+                        orders.append(Order(event.instrument, 1, 0))
                         self.buying = 1
                     self.accel = self.accel_min
                 else:
@@ -61,13 +67,31 @@ class PedroStrategy(Strategy):
 
 
 class PedroIntr(Strategy):
-    
+
+    def __init__(self, stock=float('inf'), coin=float('inf')):
+        self.stock = stock
+        self.coin = coin
+
+    def pushStock(self, event):
+        print('stock:', event.timestamp)
+
+    def pushCoin(self, event):
+        print('coin:', event.timestamp)
+
+    def push(self, event):
+        if event.instrument == STOCK:
+            return self.pushStock(event)
+        elif event.instrument == COIN:
+            return self.pushCoin(event)
+        else:
+            raise ValueError('Event instrument not accepted')
+        return []
 
 
 def evaluateIntr(strategy, files):
     return evaluate(strategy, MarketData.INTR, files)
 
 
-print(evaluateHist(PedroStrategy(), {'BVSP': '^BVSP.csv'}))
-print(evaluateIntr(PedroIntr(), {'PETR3': 'dataPedro/ADR.csv',
-                                 'USDBRL': 'USDBRL.csv'}))
+print(evaluateHist(SAR(), {'IBOV': '^BVSP.csv'}))
+print(evaluateIntr(PedroIntr(), {STOCK: 'dataPedro/ADR.csv',
+                                 COIN: 'USDBRL.csv'}))
